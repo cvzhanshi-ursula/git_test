2f08f935cf1bd9c129abb8412d0d6207548ce3b1
cvzhanshi <cvzhanshi@qq.com>
2021-06-06 13:52:56 +0800

    first commit

---
+++ src/main/java/cn/cvzhanshi/test/Demo01.java
@@ -0,0 +1,12 @@
+package cn.cvzhanshi.test;
+
+/**
+ * @author cVzhanshi
+ * @create 2021-06-06 13:51
+ */
+public class Demo01 {
+    public static void main(String[] args) {
+        System.out.println("git test1");
+        System.out.println("git test2");
+    }
+}
