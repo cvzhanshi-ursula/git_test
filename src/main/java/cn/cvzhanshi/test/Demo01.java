e30a0a68479cb428dd7fb3b0395f384f07d7f6dd
cvzhanshi <cvzhanshi@qq.com>
2021-06-06 14:12:16 +0800

    hot-fix commit2

--- src/main/java/cn/cvzhanshi/test/Demo01.java
+++ src/main/java/cn/cvzhanshi/test/Demo01.java
@@ -9,6 +9,7 @@ public class Demo01 {
         System.out.println("git test1");
         System.out.println("git test2");
         System.out.println("git test3 hot-fix");
+        System.out.println("git test3 hot-fix2");
 
     }
 }
