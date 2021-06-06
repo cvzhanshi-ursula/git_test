6f3c4147a565f62a2ae3b23328dfd82a17da59ea
cvzhanshi <cvzhanshi@qq.com>
2021-06-06 13:53:37 +0800

    second commit

--- src/main/java/cn/cvzhanshi/test/Demo01.java
+++ src/main/java/cn/cvzhanshi/test/Demo01.java
@@ -8,5 +8,7 @@ public class Demo01 {
     public static void main(String[] args) {
         System.out.println("git test1");
         System.out.println("git test2");
+        System.out.println("git test3");
+
     }
 }
